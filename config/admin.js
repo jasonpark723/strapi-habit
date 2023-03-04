c27c6c38ef688d582c1a029114cdb98f6f30502f
jasonpark723 <jasonpark723@ucla.edu>
2023-03-04 10:34:03 -0800

    config update

--- config/admin.js
+++ config/admin.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET', 'e4350524b0689fc3f46e2407dae950e2'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: ["ZyfapGA3NZuHvXR95vAZ6EFg+9D9mXURA5wCIi1Bp5E="],
   },
 
 });

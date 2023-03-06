4b898e8c62ee031d7f32777ca9563380c39b02b9
Paul Park <paulpark330@hotmail.com>
2023-03-06 14:05:58 -0800

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'e4350524b0689fc3f46e2407dae950e2'),
+    secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: ["ZyfapGA3NZuHvXR95vAZ6EFg+9D9mXURA5wCIi1Bp5E="],
+    salt: env('API_TOKEN_SALT'),
   },
-
 });
